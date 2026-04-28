c9d68b2e73bff15392b88058b03c963ed39bbd9f
samian <sam.iannazzo@yahoo.com>
2026-04-27 20:46:52 -0700

    created student model

---
+++ COMP003B.LectureActivity6.1/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureActivity6._1.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+
+        public string Email { get; set; }
+
+        // Collection navigation property
+
+        public virtual ICollection<Enrollments>? Enrollments { get; set; }
+    }
+}
