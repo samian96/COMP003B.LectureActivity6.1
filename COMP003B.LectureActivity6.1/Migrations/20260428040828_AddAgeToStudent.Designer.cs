853ae8f0810a5b95a3b421582f31a38e645cebf6
samian <sam.iannazzo@yahoo.com>
2026-04-27 21:17:24 -0700

    added viewbag to studentcontroller

--- COMP003B.LectureActivity6.1/Migrations/20260428040828_AddAgeToStudent.Designer.cs
+++ COMP003B.LectureActivity6.1/Migrations/20260428040828_AddAgeToStudent.Designer.cs
@@ -3,6 +3,7 @@ using COMP003B.LectureActivity6._1.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace COMP003B.LectureActivity6._1.Migrations
 {
     [DbContext(typeof(WebDevAcademyContext))]
-    partial class WebDevAcademyContextModelSnapshot : ModelSnapshot
+    [Migration("20260428040828_AddAgeToStudent")]
+    partial class AddAgeToStudent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ COMP003B.LectureActivity6.1/Migrations/20260428040828_AddAgeToStudent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP003B.LectureActivity6._1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAgeToStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Students");
+        }
+    }
+}
