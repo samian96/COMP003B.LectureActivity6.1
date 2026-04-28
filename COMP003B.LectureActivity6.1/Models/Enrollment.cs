d20db9cd7cefcedb9ddbba71c7267471facae313
samian <sam.iannazzo@yahoo.com>
2026-04-27 20:50:38 -0700

    createrd model enrollment

---
+++ COMP003B.LectureActivity6.1/Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.LectureActivity6._1.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int CourseId { get; set; }
+
+        // Nullable navigation properties
+
+        public virtual Student? Student { get; set; }
+
+        public virtual Course? Course { get; set; }
+    }
+}
